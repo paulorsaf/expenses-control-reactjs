b6850502d5f1d72624a9282b285b19e1f969f003
Paulo Alves <paulorsaf@gmail.com>
2022-06-20 13:41:30 -0300

    Design da tela de login

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Paulo Alves
-        </a>
-      </header>
-    </div>
+    <main className='centralize'>
+      <form>
+        <input type="email" placeholder='Email' />
+        <input type="password" placeholder='Senha' />
+        <button type="button" className='clear'>Recuperar senha</button>
+        <button type="button" className='solid'>Entrar</button>
+        <button type="button" className='outline'>Registrar</button>
+      </form>
+    </main>
   );
 }
 
