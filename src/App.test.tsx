36381c4a754bb9b9fd5f4eaaa85b20c06d03482f
Paulo Alves <paulorsaf@gmail.com>
2022-06-22 11:03:56 -0300

    Login: Validaçao dos campos de entrada com TDD

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,72 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Login', () => {
+
+  test('given email, when empty, then show required error message', async () => {
+    render(<App />);
+
+    const email = screen.getByTestId('email');
+
+    await userEvent.type(email, "anyValue");
+    await userEvent.clear(email);
+
+    const requiredError = screen.queryByTestId('email-required');
+    expect(requiredError).not.toBeNull();
+  })
+
+  test('given email, when has value, then hide required error message', async () => {
+    render(<App />);
+
+    const email = screen.getByTestId('email');
+
+    await userEvent.type(email, "anyValue");
+
+    const requiredError = screen.queryByTestId('email-required');
+    expect(requiredError).toBeNull();
+  })
+
+  test('given email, when field not changed, then hide required error message', async () => {
+    render(<App />);
+
+    const requiredError = screen.queryByTestId('email-required');
+    expect(requiredError).toBeNull();
+  })
+
+  test('given email, when invalid, then show invalid error message', async () => {
+    render(<App />);
+
+    const email = screen.getByTestId('email');
+
+    await userEvent.type(email, "anyValue");
+
+    const requiredError = screen.queryByTestId('email-invalid');
+    expect(requiredError).not.toBeNull();
+  })
+
+  test('given password, when empty, then show required error message', async () => {
+    render(<App />);
+
+    const password = screen.getByTestId('password');
+
+    await userEvent.type(password, "anyValue");
+    await userEvent.clear(password);
+
+    const requiredError = screen.queryByTestId('password-required');
+    expect(requiredError).not.toBeNull();
+  })
+
+  test('given password, when has value, then hide required error message', async () => {
+    render(<App />);
+
+    const password = screen.getByTestId('password');
+
+    await userEvent.type(password, "anyValue");
+
+    const requiredError = screen.queryByTestId('password-required');
+    expect(requiredError).toBeNull();
+  })
+
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [form, setForm] = useState({
+    email: {
+      hasChanged: false,
+      value: ""
+    },
+    password: {
+      hasChanged: false,
+      value: ""
+    }
+  })
+
+  const isEmailValid = (email: string) => {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+
   return (
     <main className='centralize'>
       <form>
-        <input type="email" placeholder='Email' />
-        <input type="password" placeholder='Senha' />
+        <input type="email" placeholder='Email' value={form.email.value}
+          onChange={event => setForm({...form, email: {
+            hasChanged: true, value: event.target.value
+          }})}
+          data-testid='email' />
+        {
+          form.email.hasChanged && !form.email.value
+            && <div data-testid="email-required">Email é obrigatório</div>
+        }
+        {
+          form.email.hasChanged && !isEmailValid(form.email.value)
+            && <div data-testid="email-invalid">Email é inválido</div>
+        }
+        
+        <input type="password" placeholder='Senha' value={form.password.value}
+          onChange={event => setForm({...form, password: {
+            hasChanged: true, value: event.target.value
+          }})}
+          data-testid="password"/>
+        {
+          form.password.hasChanged && !form.password.value
+            && <div data-testid="password-required">Senha é obrigatória</div>
+        }
         <button type="button" className='clear'>Recuperar senha</button>
         <button type="button" className='solid'>Entrar</button>
         <button type="button" className='outline'>Registrar</button>
