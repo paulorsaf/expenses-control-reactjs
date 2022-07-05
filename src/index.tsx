d1e05f8832b7492e4d7c8137604544932e6cfa16
Paulo Alves <paulorsaf@gmail.com>
2022-07-04 21:40:22 -0300

    Adicionando firebase auth

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as firebase from 'firebase/app';
+import * as firebaseAuth from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCBSl2BdAd_DghcW5a5-qp8gFcK0nfuGCY",
+  authDomain: "controle-de-gastos-3e7f9.firebaseapp.com",
+  projectId: "controle-de-gastos-3e7f9",
+  storageBucket: "controle-de-gastos-3e7f9.appspot.com",
+  messagingSenderId: "1018055774945",
+  appId: "1:1018055774945:web:bb0b24d7c0a4992e16dda0"
+};
+const app = firebase.initializeApp(firebaseConfig);
+
+const auth = firebaseAuth.initializeAuth(app);
+firebaseAuth.signInWithEmailAndPassword(
+  auth, 'paulorsaf@gmail.com', '123456'
+)
+.then(user => console.log(user))
+.catch(error => console.log('error', error));
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
